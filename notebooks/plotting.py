44de1fa9175c96fce0e2a228ee506e2711b3e6a7
arushisinha98 <115908316+arushisinha98@users.noreply.github.com>
2022-10-25 12:54:04 +0800

    Update plotting.py

--- notebooks/plotting.py
+++ notebooks/plotting.py
@@ -14,24 +14,59 @@ from sklearn.model_selection import LinearRegression
 from IPython.core.display import display, HTML
 from tqdm.keras import TqdmCallback
 
-def plot_errors(city, model, pos = -1, ind = 01, s = 0.2):
-  if pos > 0:
-    plt.subplot(4, 2, pos)
-  maxx = np.max(ys_tests[city][:,ind])
-  minn = np.min(ys_tests[city][:,ind])
-  for model in models:
-    name = str(model.__class__).split(".")[-1][:-2]
-    ys_pred = model.predict(xs_tests[city])
-    plt.scatter(ys_pred[:,ind], ys_tests[city][:,ind], alpha = 1, s = s, label = name)
-    maxx = max(np.max(ys_pred[:,ind]),maxx)
-    minn = min(np.min(ys_pred[:,ind]),minn)
+# extract files
+files = "Sydney Tasmania Perth Adelaide".split()
+xs_tests = dict()
+ys_tests = dict()
+xs_trains = dict()
+ys_trains = dict()
+xs_valids = dict()
+ys_valids = dict()
+
+for file in files:
+  csvfile = open(f"/notebooks/WECs_dataset/{file}_Data.csv")
+  reader = csv.reader(csvfile, delimiter = ",")
+  rows = [[float(v) for v in row] for row in reader]
+  rows = no.asarray(rows, dtype = "float32")
+  xs = rows[:,0:32]
+  ys = rows[:,32:49]
+  
+  # standardize xs and ys
+  ys = ys / np.max(ys)
+  xs = xs / np.max(xs)
+  
+  xs_train, xs_test, ys_train, ys_test = train_test_split(xs, ys, test_size = 0.2, random_state = )
+  xs_trains[file] = xs_train
+  ys_trains[file] = ys_train
+  xs_tests[file] = xs_test
+  ys_tests[file] = ys_test
+  with open(f"/notebooks/models/{file}_hist.pickle","rb") as f:
+    p = pickle.load(f)
+  xs_valids[file] = p.validation_data[0]
+  ys_valids[file] = p.validation_data[1]
+
+def get_error(file, model):
+  """ Returns MAPE by calculating ys_pred from xs_test array and comparing to ys_test array"""
+  ys_pred = model.predcit(xs_tests[file])
+  mape = tf.keras.losses.MeanAbsolutePercentageError()
+  error = mape(ys_pred, ys_tests[file]).numpy()
+  return error
+
+def plot_errors(city, models, pos, s = 0.2):
+  for ind in range(0,17):
+    plt.subplot(4, 17, pos)
+    for model in models:
+      name = str(model.__class__).split(".")[-1][:-2]
+      ys_pred = model.predict(xs_test[city])
+      plt.scatter(ys_pred[:,ind], ys_test[city][:,ind], alpha = 1, s = s, label = name)
+      maxx = np.max(ys_pred[:,ind])
+      minn = np.min(ys_pred[:,ind])
+    plt.legend()
+    plt.title(f"{city} WEC #{ind}" if ind <= 16 else f"{city} Total WECs Output")
+    plt.axis(t + t)t
+    plt.plot(t,t,'r--')
   plt.xlabel("Predicted Energy Ouptut (Normalized)")
   plt.ylabel("Actual Energy Output (Normalized)")
-  plt.legend()
-  plt.title(f"{city} WEC #{ind}" if ind > 0 else f"{city} Total WECs Output")
-  t = [minn, maxx]
-  plt.axis(t + t)
-  plt.plot(t,t,'r--')
   
 plt.rcParams['figure.figsize'] = [12, 16]
 i = 0
@@ -39,7 +74,7 @@ i = 0
 table_data = []
 for city in cities:
   sequential_model = models.load_model(f"{city}_model.h5")
-  linear_model = LinearRegression().fit(xs_trains[city], ys_trains[city])
+  linear_model = LinearRegression().fit(xs_train[city], ys_train[city])
   models = [linear_model, sequential_model]
   
   plot_errors(file, models, pos = i+1, ind = 0)
